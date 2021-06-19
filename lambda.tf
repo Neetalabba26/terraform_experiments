47cad25b58da30725d58b087667bb2f68277d925
Neetalabba26 <83055786+Neetalabba26@users.noreply.github.com>
2021-06-19 11:50:04 +0530

    updated lambda.tf with provider

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
   assume_role_policy = <<EOF
